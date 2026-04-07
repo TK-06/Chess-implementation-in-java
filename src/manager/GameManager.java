02c44fd8a9e19fc5c83acba574707f29384fcdc6
Taj Kongsaeree <Palangtaj@gmail.com>
2026-04-07 14:33:19 +0700

    1.2.2 Minor Bug Fix

--- src/manager/GameManager.java
+++ src/manager/GameManager.java
@@ -13,7 +13,7 @@ import java.util.Stack;
 
 public class GameManager {
 
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.2";
 
     private static GameManager instance = null;
     private Board board;
